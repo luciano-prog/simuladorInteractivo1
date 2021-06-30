9dbd2209f78c3d3203862871809cbecc67740e6b
Your Nameclear <you@example.com>
2021-06-30 19:26:52 -0300

    my first commit

---
+++ manejador2.js
@@ -0,0 +1,28 @@
+let consulta = prompt(
+  "quiere ingresar a nuestro torneo de futbol?"
+).toUpperCase();
+
+alert(" Disfruta del futbol y recomendanos a tus amigos⚽");
+
+/*while (consulta.toUpperCase() != "ESC") 
+  
+
+  switch (consulta.toUpperCase()) {
+    case "SI":
+        let nombre = prompt ("ingrese el nombre de su equipo a inscribir");
+        document.write("🎉Felicitaciones!"+ nombre + "  será parte del torneo😁 los estaremos contactando");
+        console.log(nombre + " pertenece al torneo👍");
+        break;
+    case "NO": 
+      let consulta2 = prompt("quieres jugar individualmente para un equipo").toUpperCase()
+      if (consulta2.toUpperCase() = "SI") {
+        console.log( "Contactanos al teléfono detallado más abajo")
+        document.write("Contactanos a la brevedad 📧");}
+      else {
+        document.write(" Disfruta del futbol y recomendanos a tus amigos⚽");
+      } 
+      break;   
+    default:
+        document.write(" Disfruta del futbol y recomendanos a tus amigos⚽");  
+      break;
+  }*/
---
+++ manejador3.js
@@ -0,0 +1,25 @@
+let liga = prompt(
+  "Elige la divisional, el precio cambiará si quiere jugar en la divisional A o en la divisional B"
+).toUpperCase();
+
+function precio (liga) {
+    if (liga.toUpperCase() = "A") {
+        return alert("deberán pagar $4000 la inscripción")
+      } else 
+      {alert("deberán pagar $2000 la inscripción")
+    }
+}
+
+
+/*let precio = (divi, iva) => {
+  return divi * iva;
+};
+const iva = 0.21;
+
+if (liga.toUpperCase() = "A") {
+  let divi = 100;
+  alert(precio);
+} else {
+  let divi = 50;
+  alert(precio);
+}*/
